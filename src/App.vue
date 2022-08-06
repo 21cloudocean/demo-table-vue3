bebd50d130901b81a3ff1d4787efd19c238d5f6a
21cloudocean <437508630@qq.com>
2022-08-06 22:35:29 +0800

    MyTable.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,33 @@
 <template>
   <div>
     <h1>App 根组件</h1>
+    <!-- 使用表格组件 -->
+    <my-table :data="goodslist">
+      <template #header>
+        <th>#</th>
+        <th>商品名称</th>
+        <th>价格</th>
+        <th>标签</th>
+        <th>操作</th>
+      </template>
+
+      <template #body="{ row, index }">
+        <td>{{ index + 1 }}</td>
+        <td>{{ row.goods_name }}</td>
+        <td>￥{{ row.goods_price }}</td>
+        <td>{{ row.tags }}</td>
+        <td>
+          <button type="button" class="btn btn-danger btn-sm">删除</button>
+        </td>
+      </template>
+    </my-table>
   </div>
 </template>
 <script>
+import MyTable from './components/my-table/MyTable.vue'
+
 export default {
+  components: { MyTable },
   name: 'MyApp',
   data() {
     return {
---
+++ src/components/my-table/MyTable.vue
@@ -0,0 +1,33 @@
+<template>
+  <table class="table table-bordered table-striped">
+    <!-- 表格的标题区域 -->
+    <thead>
+      <tr>
+        <!-- 命名插槽 -->
+        <slot name="header"></slot>
+      </tr>
+    </thead>
+    <!-- 表格的主体区域 -->
+    <tbody>
+      <!-- 使用 v-for 指令，循环渲染表格的数据行 -->
+      <tr v-for="(item, index) in data" :key="item.id">
+        <!-- 为数据行的 td 预留的作用域插槽 -->
+        <slot name="body" :row="item" :index="index"></slot>
+      </tr>
+    </tbody>
+  </table>
+</template>
+<script>
+export default {
+  name: 'MyTable',
+  props: {
+    // 表格的数据源
+    data: {
+      type: Array,
+      required: true,
+      default: []
+    }
+  }
+}
+</script>
+<style lang="less" scoped></style>
